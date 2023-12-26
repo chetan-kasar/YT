711554123ec06656c0331fb9be196ad8d9c9c152
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-12-27 01:22:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,73 +1,56 @@
-import { useState } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [name, setName] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-axios.defaults.withCredentials = true;
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('https://yt-umber.vercel.app/register', {name, email, password})
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+const YourComponent = () => {
+
+  const [imageSrc, setImageSrc] = useState("");
+
+  const imageSelected = async (event)=>{
+  
+    /*const formData = new FormData();
+    formData.append('titleImage', event.target.files[0]);
+
+      try {
+        let response = await axios.post('http://localhost:9000/imgUpload', formData,{headers: {"Content-Type": "multipart/form-data"}});
+      } catch (error) {
+          console.error('Error sending username:', error);
+        }*/
   }
-  return (
-    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
-      <div className="bg-white p-3 rounded w-25">
-        <h2>Register</h2>
-        <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Name</strong>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Name"
-              autoComplete="off"
-              name="email"
-              className="form-control rounded-0"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              autoComplete="off"
-              name="email"
-              className="form-control rounded-0"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              className="form-control rounded-0"
-              onChange={(e) => setPassword(e.target.value)}          
-            />
-          </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">
-            Register
-          </button>
-          <p>Already Have an Account</p>
-          <button className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-            Login
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
 
-export default App;
+
+  const handleImageUpload = (event) => {
+    const selectedFile = event.target.files[0];
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    console.log(selectedFile);
+
+    try {
+      axios.post('http://localhost:5000/uploadImage', formData)
+    } catch (error) {
+        console.error('Error geting data');
+      }
+
+  };
+
+  const getData = async ()=>{
+    try {
+      await axios.get('http://localhost:5000/getImage').then(
+        response=>{
+          setImageSrc(response.data);
+      });
+
+    } catch (error) {
+        console.error('Error geting data : ', error);
+      }
+  }
+
+    return (
+      <div>
+        <input type="file" id="imageUpload" name="image" accept="image/*" onChange={handleImageUpload}></input>
+        {imageSrc && <img src={imageSrc} width={100} height={100} alt="Uploaded"/>}
+        <button onClick={getData}>Get Data</button>
+      </div>
+    );
+};
+
+export default YourComponent;
